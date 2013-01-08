2ce886f7295ef34b5771ff2589800c493645f808
David Gross <david.alexander.gross@gmail.com>
2013-01-07 19:50:56 -0800

    updated comments in prep for refactor / upgrade
    We want to start using a Gene class in order to support choosing which
    direction we travel in time.

--- math_genes.m
+++ math_genes.m
@@ -1,7 +1,7 @@
-function varargout = math_genes(varargin)
+function varargout = math_genes(startGene,doFollowTime)
 % MATH_GENES crawls the AMS Math Genealogy Project into the past
 %
-% *Usage*
+%USAGE
 %
 %   math_genes() will crawl into the past from a given node or person and
 %   create a list of the history starting from that node or person.
@@ -12,50 +12,64 @@ function varargout = math_genes(varargin)
 %   math_genes(PERSON_NAME) will crawl from the first match of a person's
 %   name in their search results.
 %
-% *Revision Info*
+%   math_genes(START_GENE,DOFOLLOWTIME) will crawl in the direction specified.
+%   DOFOLLOWTIME defaults to false, meaning the default behavior is to
+%   travel into the past.
+%
+%AUTHORSHIP
 %   Created By  : David Gross
 %   Created On  : 2012 Jun 15, 17:46
 %
-% *See Also* urlread, regexp, regexpi
+%See Also
+%   urlread, regexp, regexpi
 
+%% Set up the base URLs for the AMS Geneology
 mirror{1} = 'http://www.genealogy.math.ndsu.nodak.edu/'; % NDSU
 mirror{2} = 'http://www.genealogy.ams.org/'; % AMS
 mirror{3}  ='http://www.math.uni-bielefeld.de/genealogy/'; % Bielefeld
 mirror{4} = 'http://genealogy.impa.br/'; % IMPA
-baseUrl = mirror{3};
 
+%% Set up the shared variables
+baseUrl = mirror{1};
 tab = '';
 
-if nargin ~=1
+%% Handle Inputs
+if nargin < 1
     error([upper(mfilename) ':InvalidInput'], ...
         'Only one input is supported');
-else
-    if isnumeric(varargin{1})
-        url = sprintf([baseUrl 'id.php?id=%i'],varargin{1});
-    elseif ischar(varargin{1})
-        error([upper(mfilename) ':InvalidInput'], ...
-            'Actually we don''t support string names yet.');
-    else
-        error([upper(mfilename) ':InvalidInput'], ...
-            'Only char and numeric inputs are supported');
-    end
-    %     url = [baseUrl randomTag];
-    %     % fprintf(1,'Finding retreival info...\n')
-    %     str = urlread(url);
-    %     current = regexpi(str, ...
-    %         'Retrieved from "<a href="(?<link>[^"]+)"','names');
-    %     if numel(current) ~= 1
-    %         warning('Did not find random site name')
-    %         keyboard
-    %     else
-    %         noamp = regexp(current(1).link,'(?<link>[^&]+)&','names');
-    %         history{1} = noamp(1).link;
-    %     end
 end
 
-% iterate
+if nargin < 2, doFollowTime = false; end
+
+%% Construct start URL based on user input
+if isnumeric(startGene)
+    url = sprintf([baseUrl 'id.php?id=%i'],startGene);
+elseif ischar(startGene)
+    error([upper(mfilename) ':InvalidInput'], ...
+        'Actually we don''t support string names yet.');
+else
+    error([upper(mfilename) ':InvalidInput'], ...
+        'Only char and numeric inputs are supported');
+end
+
+%% Old code to potentially be awesome
+%     url = [baseUrl randomTag];
+%     % fprintf(1,'Finding retreival info...\n')
+%     str = urlread(url);
+%     current = regexpi(str, ...
+%         'Retrieved from "<a href="(?<link>[^"]+)"','names');
+%     if numel(current) ~= 1
+%         warning('Did not find random site name')
+%         keyboard
+%     else
+%         noamp = regexp(current(1).link,'(?<link>[^&]+)&','names');
+%         history{1} = noamp(1).link;
+%     end
+
+%% Iterate
 history = crawl_history(url);
 
+%% Handle Outputs
 if nargout <= 1
     varargout{1} = history;
 else
@@ -63,6 +77,7 @@ else
         'No more than one output is supported');
 end
 
+%% FUNCTION CRAWL_HISTORY downloads a gene and recurses
     function history = crawl_history(url)
         history = download_gene(url);
         if isempty(history.advisor)
@@ -79,6 +94,7 @@ end
         end
     end
 
+%% FUNCTION DOWNLOAD_GENE grabs gene information for future crawling
     function gene = download_gene(url)
         % let us know what is going on while downloading data
         % fprintf(1,'%s\n',url)
@@ -90,8 +106,8 @@ end
         if numel(s) == 1 && numel(s) == numel(e)
             paddingWrapper = str(s:e);
         else
-            warning('Trouble finding paddingWrapper')
-            keyboard
+            error([upper(mfilename) ':download_gene:HtmlParsing'], ...
+                'Trouble finding paddingWrapper')
         end
         
         gene = regexpi(paddingWrapper,[...
@@ -112,7 +128,8 @@ end
             ],'names');
         
         if isempty(gene)
-            keyboard
+            error([upper(mfilename) ':download_gene:HtmlParsing'], ...
+                'Gene was empty after parsing.')
         end
         
         flag = regexpi(paddingWrapper,[ ...
@@ -138,7 +155,7 @@ end
         for a = 1:numel(gene.advisor)
             gene.advisor(a).url = [baseUrl gene.advisor(a).url];
             for f = 2:numel(fNames)
-               gene.advisor(a).(fNames{f}) = ''; 
+                gene.advisor(a).(fNames{f}) = '';
             end
             gene.advisor(a) = orderfields(gene.advisor(a),fNames);
         end
@@ -149,6 +166,7 @@ end
         
     end
 
+%% FUNCTION PRINT_GENE just prints a gene to the screen
     function print_gene(gene)
         fprintf(1,'%s%s, %s %s in %s\n', ...
             tab,gene.name,gene.institution,gene.degree,gene.year);
