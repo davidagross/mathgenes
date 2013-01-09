d72eb39792b05685ffd088bd8ff6e16bd652fcf8
David Gross <david.alexander.gross@gmail.com>
2013-01-08 20:27:45 -0800

    greatly enhanced printing and downloading
    short and long formats of printing as well as downloading of a
    generation above or below the current gene.

--- MathGene.m
+++ MathGene.m
@@ -1,21 +1,25 @@
-classdef MathGene
+classdef MathGene < handle
     %MATHGENE is a class used for travelling through the AMS Math Geneology
     
     properties
         mirrorIdx = 3; % which mirror we'll be using to download from
-        tab = '';      % how far over we will tab when printing the gene
+        ID = '';       % the node identifier in the tree
         url = '';      % the web location of the gene
         name = '';     % the name of the person in the tree
-        degree = '';   % what degrees they received
+        degree = 'not downloaded';   % what degrees they received
         institution = '';  % the institution from which they received it
         year = [];     % the year in which they received it
         nationality = '';  % the nationality of the person
-        dissertation = ''; % the title of their dissertation
+        dissertation = 'not downloaded'; % the title of their dissertation
         advisor = [];  % the person's advisors
         student = [];  % the person's students
     end
     
-    properties (SetAccess = private)
+    properties (Dependent = true)
+        descendents = [];  % the number of students
+    end
+    
+    properties (Access = private)
         mirror = { ... % various mirrors of the AMS Math Geneology Project
             'http://www.genealogy.math.ndsu.nodak.edu/'; ...   % NDSU
             'http://www.genealogy.ams.org/'; ...               % AMS
@@ -27,21 +31,63 @@ classdef MathGene
         function obj = MathGene(varargin)
             %MATHGENE(ID) will create a node from an id:
             %    http://www.genealogy.ams.org/id.php?id=ID
+            %MATHGENE() will create an empty math gene
             
-            %% Handle input
-            if nargin >= 1
-                ID = varargin{1};
-                if ~isnumeric(ID)
-                    error([upper(mfilename) ':InvalidInput'], ...
-                        'Only numeric inputs are supported');
+            %% Handle input and construct
+            if nargin == 0
+                obj.name = 'nobody';
+                obj.degree = 'nothing';
+                obj.institution = 'nowhere';
+                obj.year = 'never';
+                obj.dissertation = 'none';
+            elseif nargin >= 1
+                if isnumeric(varargin{1}) || ischar(varargin{1})
+                    % set the ID
+                    obj.ID = varargin{1};
+                    % Download the gene
+                    obj = downloadGene(obj);
+                elseif isstruct(varargin{1})
+                    props = properties(obj);
+                    fields = fieldnames(varargin{1});
+                    valid = intersect(props,fields);
+                    for v = 1:numel(valid)
+                        obj.(valid{v}) = varargin{1}.(valid{v});
+                    end
+                else
+                    error([upper(mfilename) ':InvalidInput'],[ ...
+                        'Only numeric, string, and struct inputs ' ...
+                        'are supported']);
                 end
+                
             else
                 error([upper(mfilename) ':InvalidInput'], ...
-                        'Only 1 input supported.');
+                    'Only up to 1 input supported.');
             end
-            
-            %% Download the gene
-            obj = download_gene(obj,obj.urlFromId(ID));
+        end
+        
+        function set.ID(obj,val)
+            %SET.ID sets the ID to be a number, always
+            if isnumeric(val)
+                obj.ID = val;
+            elseif ischar(val)
+                obj.ID = str2double(val);
+            else
+                error([upper(mfilename) ':set:ID'], ...
+                    'Only numeric and string IDs are supported');
+            end
+        end
+        
+        function val = get.descendents(obj)
+            %GET.DESCENDENTS returns the number of students
+            S = numel(obj.student);
+            if S ~= 0, val = num2str(S); else val = ''; end
+        end
+        
+        function set.descendents(obj,val)
+            %SET.DESCENDENTS sets the number of students in absence of data
+            val = str2double(val);
+            if ~isfinite(val), val = 0; end
+            obj.student = repmat(MathGene(),1,val);
         end
         
         function val = getMirror(obj)
@@ -49,40 +95,68 @@ classdef MathGene
             val = obj.mirror{obj.mirrorIdx};
         end
         
-        function val = urlFromId(obj,ID)
+        function val = urlFromId(obj,varargin)
             %URLFROMID creates a person URL from a node ID
-            val = sprintf([obj.getMirror() 'id.php?id=%i'],ID);
+            if isa(obj,'MathGene')
+                val = sprintf([obj.getMirror() ...
+                    'id.php?id=%i'],obj.ID);
+            else
+                val = sprintf([obj.getMirror() ...
+                    'id.php?id=%i'],varargin{1});
+            end
         end
         
-        function obj = download_gene(obj,url)
-            %DOWNLOAD_GENE grabs gene information for future crawling
+        function obj = downloadStudents(obj)
+            %DOWNLOADSTUDENTS grabs the gene information for the students
+            S = numel(obj.student);
+            for s = 1:S
+                fprintf('Downloading Student %i of %i\n',s,S);
+                obj.student(s) = obj.student(s).downloadGene();
+            end
+        end
+        
+        function obj = downloadAdvisors(obj)
+            %DOWNLOADADVISORS grabs the gene information for the advisors
+            A = numel(obj.advisor);
+            for a = 1:A
+                fprintf('Downloading Advisor %i of %i\n',a,A);
+                obj.advisor(a) = obj.advisor(a).downloadGene();
+            end
+        end
+        
+        function obj = downloadGene(obj)
+            %DOWNLOADGENE grabs gene information for future crawling
             
             % grab the web data
-            obj.url = url;
-            str = urlread(obj.url);
+            str = urlread(obj.urlFromId());
             
             % exract the padding wrapper
-            [s,e] = regexpi(str,...
-                '<div id="paddingWrapper">(.*?)<!-- end #paddingWrapper -->');
+            [s,e] = regexpi(str,[...
+                '<div id="paddingWrapper">(.*?)' ...
+                '<!-- end #paddingWrapper -->']);
             if numel(s) == 1 && numel(s) == numel(e)
                 paddingWrapper = str(s:e);
             else
-                error([upper(mfilename) ':download_gene:HtmlParsing'], ...
+                error([upper(mfilename) ':downloadGene:HtmlParsing'], ...
                     'Trouble finding paddingWrapper')
             end
             
             % extract the relevant person information
             ret = regexpi(paddingWrapper,[...
                 '<h2[^>]*>\s+(?<name>.*)\s+</h2>.*'...
-                '<div[^>]*>\s+<span[^>]*>(?<degree>[^<]*)\s+<span[^>]*>' ...
-                '(?<institution>[^<]*)</span>\s*(?<year>[^<]*)</span>.*' ...
-                '<div[^>]*><span[^>]*>Dissertation:</span> <span[^>]*' ...
-                'id="thesisTitle">\s+(?<dissertation>[^<]*)</span></div>.*' ...
+                '<div[^>]*>\s+<span[^>]*>' ...
+                '(?<degree>[^<]*)\s+<span[^>]*>' ...
+                '(?<institution>[^<]*)' ...
+                '</span>\s*(?<year>[^<]*)</span>.*' ...
+                '<div[^>]*><span[^>]*>' ...
+                'Dissertation:</span> <span[^>]*' ...
+                'id="thesisTitle">\s+' ...
+                '(?<dissertation>[^<]*)</span></div>.*' ...
                 ],'names');
             
             % parse error checking
             if isempty(ret)
-                error([upper(mfilename) ':download_gene:HtmlParsing'], ...
+                error([upper(mfilename) ':downloadGene:HtmlParsing'], ...
                     'Gene was empty after parsing.')
             end
             
@@ -104,32 +178,93 @@ classdef MathGene
             end
             
             % extract advisor information
-            obj.advisor = regexpi(paddingWrapper,[ ...
+            advisorarray = regexpi(paddingWrapper,[ ...
                 'Advisor[^<]*<a href="id.php\?id=(?<ID>[^"]*)">' ...
                 '(?<name>[^<]*)</a>', ...
                 ],'names');
-            for a = 1:numel(obj.advisor)
-                obj.advisor(a).url = obj.urlFromId(obj.advisor(a).ID);
+            obj.advisor = MathGene();
+            for a = 1:numel(advisorarray)
+                advisorarray(a).url = obj.urlFromId(advisorarray(a).ID);
+                obj.advisor(a) = MathGene(advisorarray(a));
             end
             
             % extract student information
-            obj.student = regexpi(paddingWrapper,[ ...
+            studentarray = regexpi(paddingWrapper,[ ...
                 '<tr[^>]*><td>' ...
                 '<a href="id.php\?id=(?<ID>[^"]*)">' ...
                 '(?<name>[^<]*)</a></td><td>'...
                 '(?<institution>[^<]*)</td>' ...
                 '<td[^>]*>(?<year>[^<]*)</td>' ...
                 '<td[^>]*>(?<descendents>[^<]*)</td></tr>'],'names');
-            for s = 1:numel(obj.student)
-                obj.student(s).url = obj.urlFromId(obj.student(s).ID);
+            obj.student = MathGene();
+            for s = 1:numel(studentarray)
+                studentarray(s).url = obj.urlFromId(studentarray(s).ID);
+                obj.student(s) = MathGene(studentarray(s));
             end
-            
         end
         
         function disp(obj)
             %DISP just prints a gene to the screen
-            fprintf(1,'%s%s, %s %s in %s\n', ...
-                obj.tab,obj.name,obj.institution,obj.degree,obj.year);
+            O = numel(obj);
+            for o = 1:O
+                fprintf(1,'   MathGene of %s:\n\n',obj(o).name);
+                fprintf(1,'     %s %s %s %s\n', ...
+                    obj(o).degree,obj(o).institution, ...
+                    obj(o).year,obj(o).nationality);
+                fprintf(1,'     Dissertation: %s\n\n',obj(o).dissertation);
+                if strncmp(get(0,'Format'),'long',4)
+                    % print long-hand advisors
+                    A = numel(obj(o).advisor);
+                    if A > 1
+                        for a = 1:A
+                            fprintf(1,'     Advisor %i: %s\n',...
+                                a,obj(o).advisor(a).name);
+                        end
+                    elseif A == 1
+                        fprintf(1,'     Advisor: %s\n', ...
+                            obj(o).advisor.name);
+                    else
+                        fprintf(1,'     0 Advisors\n');
+                    end
+                    
+                    % print long-hand students
+                    S = numel(obj(o).student);
+                    if S == 1, plural = ''; else plural = 's'; end;
+                    if S ~= 0, plural = cat(2,plural,':'); end
+                    fprintf(1,['\n     %i Student' plural '\n\n'],S);
+                    if S > 0
+                        f = cellfun(@(s)['%' num2str(s) 's'],num2cell( ...
+                            cellfun(@(c)max(cellfun(@numel,c)),{ ...
+                            [{obj(o).student.name} 'name'], ...
+                            [{obj(o).student.institution} 'institution'], ...
+                            [{obj(o).student.year} 'year'], ...
+                            {obj(o).student.descendents}})), ...
+                            'UniformOutput',0);
+                        d = ' | ';
+                        fprintf( ...
+                            [f{1} d f{2} d f{3} d f{4} '\n'], ...
+                            'Name','Institution','Year','Descendents');
+                        for s = 1:S
+                            fprintf([f{1} d f{2} d f{3} d f{4} '\n'], ...
+                                obj(o).student(s).name, ...
+                                obj(o).student(s).institution, ...
+                                obj(o).student(s).year, ...
+                                obj(o).student(s).descendents)
+                        end
+                    end
+                else
+                    % print short-hand advisors
+                    A = numel(obj(o).advisor);
+                    if A == 1, plural = ''; else plural = 's'; end;
+                    fprintf(1,['     %i Advisor' plural ', '],A);
+                    
+                    % print short-hand students
+                    S = numel(obj(o).student);
+                    if S == 1, plural = ''; else plural = 's'; end;
+                    fprintf(1,['%i Student' plural '\n'],S);
+                end
+                fprintf('\n');
+            end
         end
         
     end
